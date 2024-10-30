6c1a6fd84df4f0507265d4fd7b85879a045712c2
Mohammad Jafari <jafarim@gmail.com>
2024-10-30 08:58:35 -0700

    fix the db config (#6)
    * fix the db config

--- test/lib/db.test.js
+++ test/lib/db.test.js
@@ -4,3 +4,9 @@ const { sql } = require("drizzle-orm");
 it("database is connected", async () => {
   await db.execute(sql`select 1`);
 });
+
+it("tables exist is connected", async () => {
+  await db.execute(sql`select 1 from codes;`);
+  await db.execute(sql`select 1 from code_group_mappings;`);
+  await db.execute(sql`select 1 from metadata;`);
+});
